a1319856ace3d568102b5deafdfe2e08d6032103
zkl0330 <3203839255@qq.com>
2025-04-06 17:17:52 +0800

    Site updated: 2025-04-06 17:17:49

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1743928862089";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1743930914815";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
